ad146ee72a092b2df70de6590c7edcdbaf2b1cc3
Yasen Dimitrov <y_dimitrov@ymail.com>
2022-12-14 16:37:47 +0200

    Add 16x16 grid and styles

---
+++ main.js
@@ -0,0 +1,13 @@
+//UI
+const container = document.querySelector('#container');
+
+for (let i = 1; i < 257; i++) {
+    const square = document.createElement('div');
+    square.setAttribute('style', 'height: 35px; width: 35px; margin: 0px; padding: 0px; font-size: 8px; text-align: center; border: 1.2px solid black; background-color: rgb(90, 140, 140);');
+    container.appendChild(square);
+}
+
+
+
+
+
