703688ed86c5bf0d3309b206540d4a6fd344c0b5
Yasen Dimitrov <y_dimitrov@ymail.com>
2022-12-14 19:39:51 +0200

    Add event listener and drawing functionality

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 //UI
 const container = document.querySelector('#container');
 
+//create 16x16 grid
 for (let i = 1; i < 257; i++) {
     const square = document.createElement('div');
-    square.setAttribute('style', 'height: 35px; width: 35px; margin: 0px; padding: 0px; font-size: 8px; text-align: center; border: 1.2px solid black; background-color: rgb(90, 140, 140);');
+    square.classList.add('square');
     container.appendChild(square);
-}
+};
 
+function draw(e) {
+    if (e.which == 1) {
+        this.classList.add('hover-effect');
+    };
+};
 
+const children = container.childNodes;
+children.forEach(child => child.addEventListener("mousemove", draw));
 
 
 
