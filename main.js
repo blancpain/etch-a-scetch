12345a14cd584e3a77484fa7b0834188749fa149
Yasen Dimitrov <y_dimitrov@ymail.com>
2022-12-15 14:49:28 +0200

    Add color wheel and allow user to draw in different colors
    Add DOM input element created with JS and styled with CSS. Attach event listener to the element and passing the return to the drawing event handler to allow users to use different colors on demand.

--- main.js
+++ main.js
@@ -5,35 +5,16 @@ const containerStyles = getComputedStyle(container);
 const GRID_WIDTH = Number(containerStyles["max-width"].split("p")[0]);
 const GRID_HEIGHT = Number(containerStyles["max-height"].split("p")[0]);
 
-function createGrid(squares=16) {
-    container.innerHTML = '';
-    for (let i = 1; i < (squares * squares) + 1; i++) {
-        const square = document.createElement('div');
-        const squareWidth = GRID_WIDTH / squares;
-        const squareHeight = GRID_HEIGHT / squares;
-        //configure container to accept relevant number of squares
-        container.setAttribute('style', `grid-template-columns: repeat(${squares}, 1fr)`);
-        //set square dimensions to fit container
-        square.setAttribute('style', `width: ${squareWidth}px; height: ${squareHeight}px;`);
-        container.appendChild(square);
-    };
-    //add listener on mousemove to draw in squares 
-    const children = container.childNodes;
-    children.forEach(child => child.addEventListener("mousemove", draw));
-}
-createGrid();
-
-//drawing
-function draw(e) {
-    //consider changing as which has been deprecated
-    if (e.which == 1) {
-        this.classList.add('hover-effect');
-    };
-};
-
 //button related
 const buttonPanel = document.querySelector('#button-panel');
 const promptForSizeOfGrid = document.createElement('button');
+//color picker
+const colorPicker = document.createElement('input');
+colorPicker.type = "color";
+colorPicker.id = "colorpicker";
+colorPicker.value = "#610A0A";
+buttonPanel.appendChild(colorPicker);
+//change size
 promptForSizeOfGrid.textContent = "Change grid";
 promptForSizeOfGrid.classList.add('buttons');
 promptForSizeOfGrid.addEventListener('click', () => {
@@ -45,9 +26,41 @@ promptForSizeOfGrid.addEventListener('click', () => {
     };
 });
 buttonPanel.appendChild(promptForSizeOfGrid);
+//reset
+const resetGrid = document.createElement('button');
+resetGrid.textContent = "Clear";
+resetGrid.classList.add('buttons');
+resetGrid.addEventListener('click', () => {
+        createGrid();
+});
+buttonPanel.appendChild(resetGrid);
 
-
-
-
-
-
+//create and update grid; perhaps can place drawing functionality in another function
+function createGrid(squares=16) {
+    container.innerHTML = '';
+    colorPicker.value = "#610A0A";
+    let color = "#610A0A";
+    //listen for changes in default color options
+    colorPicker.addEventListener('input', () => {
+        color = colorPicker.value;
+    })
+    for (let i = 1; i < (squares * squares) + 1; i++) {
+        const square = document.createElement('div');
+        const squareWidth = GRID_WIDTH / squares;
+        const squareHeight = GRID_HEIGHT / squares;
+        //configure container to accept relevant number of squares
+        container.setAttribute('style', `grid-template-columns: repeat(${squares}, 1fr)`); 
+        //set square dimensions to fit container
+        square.setAttribute('style', `width: ${squareWidth}px; height: ${squareHeight}px;`);
+        container.appendChild(square);
+    };
+    //add listener on mouse click+move and change drawing color according to user inputs 
+    const children = container.childNodes;
+    children.forEach(child => child.addEventListener("mousemove", (e) => {
+        //consider changing as which has been deprecated
+        if (e.which == 1) {
+            child.setAttribute('style', `background-color: ${color};`);
+        };
+    }));  
+};
+createGrid();
