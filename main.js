88ca46e86a9597f0b2e8035b0fa91fd24c6e593c
Yasen Dimitrov <y_dimitrov@ymail.com>
2022-12-15 11:56:18 +0200

    Configure createGrid to accept any number of squares withouth overflow

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 //grid related
 const container = document.querySelector('#container');
+const containerStyles = getComputedStyle(container);
+//getComputedStyle returns size in ...px so need to convert to number before using 
+const GRID_WIDTH = Number(containerStyles["max-width"].split("p")[0]);
+const GRID_HEIGHT = Number(containerStyles["max-height"].split("p")[0]);
+
 function createGrid(squares=16) {
     container.innerHTML = '';
     for (let i = 1; i < (squares * squares) + 1; i++) {
         const square = document.createElement('div');
-        square.classList.add('square');
+        const squareWidth = GRID_WIDTH / squares;
+        const squareHeight = GRID_HEIGHT / squares;
+        //configure container to accept relevant number of squares
+        container.setAttribute('style', `grid-template-columns: repeat(${squares}, 1fr)`);
+        //set square dimensions to fit container
+        square.setAttribute('style', `width: ${squareWidth}px; height: ${squareHeight}px;`);
         container.appendChild(square);
     };
     //add listener on mousemove to draw in squares 
