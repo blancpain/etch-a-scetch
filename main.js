cb8b9919b5128691f9fbc95988828e72002bf5ed
Yasen Dimitrov <y_dimitrov@ymail.com>
2022-12-17 00:13:33 +0200

    Change logic of grid creation
    Add variable to css file to hold number of rows/columns for the createGrid function depending on user input (if any, if not uses the default number of squares to evaluation rows/columns).
    
    This has corrected an error where a fully drawn out grid makes the grid disappear. I believe the issue was happening as the previous logic was to calculate number of squares on the fly and updated/replace the css styling for each square. Then the styling was getting overridden by the drawing functionality and essentially made the divs disappear.

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 //grid related
-const container = document.querySelector('#container');
-const containerStyles = getComputedStyle(container);
-//getComputedStyle returns size in ...px so need to convert to number before using 
-const GRID_WIDTH = Number(containerStyles["max-width"].split("p")[0]);
-const GRID_HEIGHT = Number(containerStyles["max-height"].split("p")[0]);
+const container = document.querySelector('#grid');
 const numOfSquares = 30;
-//button related
+//buttons
 const buttonPanel = document.querySelector('#button-panel');
 //color picker
 const colorPicker = document.createElement('input');
@@ -16,7 +12,7 @@ buttonPanel.appendChild(colorPicker);
 //range slider to change size
 const promptForSizeOfGrid = document.createElement('input');
 promptForSizeOfGrid.type = "range";
-promptForSizeOfGrid.min = "16";
+promptForSizeOfGrid.min = "8";
 promptForSizeOfGrid.max = "100";
 promptForSizeOfGrid.id = "slider";
 promptForSizeOfGrid.value = "50 x 50";
@@ -40,29 +36,26 @@ resetGrid.addEventListener('click', () => {
 });
 buttonPanel.appendChild(resetGrid);
 
-//create and update grid; perhaps can place drawing functionality in another function
-function createGrid(squares=30) {
+//main function
+function createGrid(squares=numOfSquares) {
     container.innerHTML = '';
     //listen for changes in default color options, if no user input yet use default color
     let color = colorPicker.value;
     colorPicker.addEventListener('input', () => {
         color = colorPicker.value;
     });
+    //use the css variable "--cols" and update it dynamically to populate grid
+    let numOfCols = squares;
+    container.style.setProperty("--cols", numOfCols);
     //create grid
-    for (let i = 1; i < (squares * squares) + 1; i++) {
+    for (let i = 1; i < (numOfCols * numOfCols) + 1; i++) {
         const square = document.createElement('div');
-        const squareWidth = GRID_WIDTH / squares;
-        const squareHeight = GRID_HEIGHT / squares;
-        //configure container to accept relevant number of squares; have to set both ROWS and COLUMNS otherwise squares disappear when whole row/column is colored
-        container.setAttribute('style', `grid-template-columns: repeat(${squares}, 1fr); grid-template-rows: repeat(${squares}, 1fr)`); 
-        //set square dimensions to fit container
-        square.setAttribute('style', `width: ${squareWidth}px; height: ${squareHeight}px;`);
         container.appendChild(square);
     };
     //add listener on mouse click+move and change drawing color according to user inputs 
     const children = container.childNodes;
     children.forEach(child => child.addEventListener("mousemove", (e) => {
-        //consider changing as which has been deprecated
+        //consider changing as "which" has been deprecated
         if (e.which == 1) {
             child.setAttribute('style', `background-color: ${color};`);
         };
